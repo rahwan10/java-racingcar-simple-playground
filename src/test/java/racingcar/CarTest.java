1454dd6fbc3c64938d4a8459d09a2ff0672e9517
gyuminJJANG <ggavwu789@gmail.com>
2026-09-14 00:26:42 +0900

    randomValue 4 이상일 때 전진하는 테스트, randomValue 3 이하일 때 멈추는 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -13,4 +13,20 @@ class CarTest {
         Car car = new Car("pobi");
         assertThat(car.getName()).isEqualTo("pobi");
     }
+
+    @Test
+    @DisplayName("randomValue가 4 이상이면 전진한다")
+    void moveWhenRandomValueIsMovable() {
+        Car car = new Car("pobi");
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("randomValue가 3 이하이면 멈춘다")
+    void stopWhenRandomValueIsNotMovable() {
+        Car car = new Car("pobi");
+        car.move(3);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
 }
