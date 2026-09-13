13666d851e462864856235d824b6ea96eb723b30
gyuminJJANG <ggavwu789@gmail.com>
2026-09-14 00:47:18 +0900

    자동차 전체 이동 기능 구현

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -12,4 +12,10 @@ public class Cars {
             cars.add(new Car(name));
         }
     }
+
+    public void moveAll(List<Integer> randomValues){
+        for (int i=0;i<cars.size();i++){
+            cars.get(i).move(randomValues.get(i));
+        }
+    }
 }
