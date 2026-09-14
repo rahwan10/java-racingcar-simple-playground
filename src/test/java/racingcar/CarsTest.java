ca43b829d155ffd79243b192cf035557361f5c4a
gyuminJJANG <ggavwu789@gmail.com>
2026-09-14 12:52:48 +0900

    공동 우승자 테스트 추가

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -16,4 +16,13 @@ class CarsTest {
 
         assertThat(cars.findWinners()).containsExactly("pobi");
     }
+
+    @Test
+    @DisplayName("가장 멀리 간 자동차가 여러 대면 공동 우승한다")
+    void findMultipleWinners(){
+        Cars cars=new Cars(List.of("pobi","jun"));
+        cars.moveAll(List.of(4,4));
+
+        assertThat(cars.findWinners()).containsExactly("pobi","jun");
+    }
 }
