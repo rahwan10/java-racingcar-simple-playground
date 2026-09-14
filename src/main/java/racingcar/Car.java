a6bbae647bc9876e6d3f95f152636b0b00a3a7f1
gyuminJJANG <ggavwu789@gmail.com>
2026-09-14 14:43:34 +0900

    코드 컨벤션 적용

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,27 +1,27 @@
 package racingcar;
 
 public class Car {
-    private static final int MOVABLE_THRESHOLD=4;
+    private static final int MOVABLE_THRESHOLD = 4;
 
     private final String name;
     private int position;
 
-    public Car(String name){
-        this.name=name;
-        this.position=0;
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
     }
 
-    public void move(int randomValue){
-        if (randomValue>=MOVABLE_THRESHOLD){
+    public void move(int randomValue) {
+        if (randomValue >= MOVABLE_THRESHOLD) {
             position++;
         }
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getPosition(){
+    public int getPosition() {
         return position;
     }
 
--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -6,34 +6,34 @@ import java.util.List;
 public class Cars {
     private final List<Car> cars;
 
-    public Cars(List<String> names){
-        this.cars=new ArrayList<>();
-        for (String name : names){
+    public Cars(List<String> names) {
+        this.cars = new ArrayList<>();
+        for (String name : names) {
             cars.add(new Car(name));
         }
     }
 
-    public void moveAll(List<Integer> randomValues){
-        for (int i=0;i<cars.size();i++){
+    public void moveAll(List<Integer> randomValues) {
+        for (int i = 0; i < cars.size(); i++) {
             cars.get(i).move(randomValues.get(i));
         }
     }
 
-    private int findMaxPosition(){
-        int maxPosition=0;
-        for (Car car:cars){
-            if (car.getPosition()>maxPosition){
-                maxPosition=car.getPosition();
+    private int findMaxPosition() {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
             }
         }
         return maxPosition;
     }
 
-    public List<String> findWinners(){
-        int maxPosition=findMaxPosition();
+    public List<String> findWinners() {
+        int maxPosition = findMaxPosition();
         List<String> winners = new ArrayList<>();
-        for (Car car:cars){
-            if(car.getPosition()==maxPosition){
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
                 winners.add(car.getName());
             }
         }
