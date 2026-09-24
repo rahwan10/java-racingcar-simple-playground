ed3272c0640f44bff1ef38921f9a1bbbed8d09b7
rahwan10 <donggeon10@naver.com>
2026-09-24 23:36:31 +0900

    refactor: InputView의 반환값을 GameSettings DTO로 통합

---
+++ src/main/java/racingcar/view/GameSettings.java
@@ -0,0 +1,4 @@
+package racingcar.view;
+
+public record GameSettings(String namesInput, int tryCount) {
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public GameSettings readGameSettings() {
+        String namesInput = readCarNames();
+        int tryCount = readTryCount();
+        return new GameSettings(namesInput, tryCount);
+    }
+
     public String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.nextLine();
